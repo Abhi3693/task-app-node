d8ab4f12afebf5a5fe58219ceb8ff3d11c7e2c29
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-06-08 18:22:09 +0530

    Adds cores config

--- app.js
+++ app.js
@@ -20,19 +20,26 @@ mongoose
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Cors config
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', '*');
+//   res.setHeader('Access-Control-Allow-Headers', '*');
+//   next();
+// });
 
 // Routes
 app.use('/api/tasks', tasksRouter);
